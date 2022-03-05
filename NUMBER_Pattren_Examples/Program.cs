72c0f4b2dcef5dda4cf295c948fa444608510fc3
pranayshinde855 <pranayshinde855@gmail.com>
2022-03-05 17:35:37 +0530

    Add project files.

---
+++ NUMBER_Pattren_Examples/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NUMBER_Pattren_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 Print 1 to 5 Num bers
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\n");
+
+            //2 Print 5 to 1 numbers
+            for (int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\n");
+
+            //3 Add numbers to line by line from 1 to 5
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+
+            //4 Add Numbers in line by line from 1 to 5
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int J = 5; J >= i; J--)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Pattern_Examples/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace STAR_Pattern_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 Understood the difference between Write() & WriteLine()
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine("\n");
+
+            //2
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= 6; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine("\n");
+
+            //3
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine("\n");
+
+            //4
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine("\n");
+
+            //5
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+
+            //6
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int l = 6; l > i; l--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+
+            //7
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int l = 0; l < i; l++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+
+            //8
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 6; k > i; k--)
+                {
+                    Console.Write("*");
+                }
+                for (int l = 6; l >= i; l--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
